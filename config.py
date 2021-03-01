75e4d49900f34e1517c01ea0b5f650f691797992
0xff_51 <61901987+oxff644@users.noreply.github.com>
2021-03-01 21:28:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 class Config:
     name = "CVEMonitor"
 
-    feishu_API = "https://open.feishu.cn/open-apis/bot/v2/hook/45a80625-888e-4e29-bb3c-80055f95afd4"
+    feishu_API = "xxxxx"
 
-    mail_usr = "ak47__98k@163.com"
-    mail_pwd = "989847k"
-    mail_host = "smtp.163.com"
+    mail_usr = "xxxxx"
+    mail_pwd = "xxxxxx"
+    mail_host = "xxxxx"
     mail_port = 25
     mail_targets = [
-        "ak47__98k@163.com",
-        "wenpp@lianlianpay.com",
-        "yaoyq@lianlianpay.com",
+        "xxxxxxx",
+        "xxxxxxxx",
     ]
